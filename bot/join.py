739cd02c5b3f95eb81dbdf1a46d98d9f3c246a7d
johnpoint <jahanngauss414@gmail.com>
2018-02-11 23:50:05 +0800

    # fix  * fix message too long

--- bot/join.py
+++ bot/join.py
@@ -5,6 +5,7 @@ import linecache
 import random
 import sys
 import time
+import os
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -19,16 +20,19 @@ def add_in(user):
         f.close()
         sf = open('slist','a+w')
         s = ' | '
-        localtime = time.strftime("%H:%M:%S", time.localtime())
+        localtime = time.strftime("%m-%d|%H:%M:%S", time.localtime())
         rr = localtime + s + r
         print >> sf,rr
         sf.close()
         return 1, '成功参加抽奖~'
     else:
-        return 0, '你已经参加了哦~耐心等待开奖吧！'
+        return 0, '你已经参加了哦~请耐心等待开奖吧！'
 
 def get_lottery():
-    count = len(open('list', 'r').readlines())
+    count = -1
+    for count, line in enumerate(open("list", 'r')):
+        pass
+    count += 1
     if count == '0':
         return 0,'None'
     else:
@@ -39,15 +43,24 @@ def get_lottery():
             rr = u'我抽中了： @%s'%a
     return 1,rr
 
-def read_list():
-    f = open('slist')
-    r = f.read()
-    f.close()
-    return r
-
+def read_list(un):
+	s = os.path.getsize('slist')
+	if s < 4096:
+            with open('slist','r') as f:
+                rr = f.read()
+                return rr
+        else:
+    	    f = open('list', 'r')
+            l = f.read()
+    	    if l.find('%s' %un) == -1:
+    	        return u'由于telegram限制，我无法提供完整的名单 \n\n 但您   未参与   抽奖'
+            else:
+        	return u'由于telegram限制，我无法提供完整的名单 \n\n 但您   已参与   抽奖'
+    	
 def del_list():
     f = open('list', 'w')
     f.close()
     sf = open('slist','w')
     sf.close()
     return '名单已清空~'
+
--- bot/main.py
+++ bot/main.py
@@ -19,7 +19,7 @@ def send_welcome(message):
     else:
         bot.send_chat_action(message.chat.id, 'typing')
         msg_id = bot.send_message(message.chat.id, "你好，欢迎使用本机器人\n\n 点击 /help - 获取帮助\n").message_id
-		time.sleep(5)
+	time.sleep(5)
         bot.delete_message(message.chat.id,msg_id)
 
 @bot.message_handler(commands=['help'])
@@ -30,7 +30,7 @@ def send_welcome(message):
     else:
         bot.send_chat_action(message.chat.id, 'typing')
         msg_id = bot.send_message(message.chat.id, "/join - 加入抽奖\n/list - 查看名单\n/lottery - 抽奖[admin]\n/clear - 清空名单[admin]\n").message_id
-		time.sleep(5)
+	time.sleep(5)
         bot.delete_message(message.chat.id,msg_id)
 
 @bot.message_handler(commands=['join'])
@@ -49,16 +49,20 @@ def send_join(message):
             msg_id = bot.reply_to(message, r).message_id
         else:
             msg_id = bot.reply_to(message, r).message_id
-		time.sleep(5)
+	time.sleep(5)
         bot.delete_message(message.chat.id,msg_id)
 
 @bot.message_handler(commands=['list'])
 def send_list(message):
     if message.chat.type == 'private' :
+    	un = message.from_user.username
         bot.send_chat_action(message.chat.id, 'typing')
-        r = join.read_list()
-        num = len(open('slist','rU').readlines())
-        rr = u'%s \n\n 目前共有%s人参与抽奖哦'%(r,num)
+        r = join.read_list(un)
+        count = -1
+        for count, line in enumerate(open("list", 'r')):
+            pass
+        count += 1
+        rr = u'%s \n\n 目前共有%s人参与抽奖哦'%(r,count)
         bot.reply_to(message,rr)
     else:
         bot.send_chat_action(message.chat.id, 'typing')
@@ -66,7 +70,7 @@ def send_list(message):
         btn = types.InlineKeyboardButton('戳这里！', url = 'https://t.me/yahahaabot')
         markup.add(btn)
         msg_id = bot.send_message(chat_id=message.chat.id, text=u'为了防止刷屏，请在私聊中使用此命令哦～',reply_markup=markup).message_id
-		time.sleep(5)
+	time.sleep(5)
         bot.delete_message(message.chat.id,msg_id)
 
 
@@ -84,7 +88,7 @@ def send_welcome(message):
     else:
         if l.find('%s' %un) == -1:
             msg_id = bot.reply_to(message,'您没有权限哦').message_id
-			time.sleep(5)
+	    time.sleep(5)
             bot.delete_message(message.chat.id,msg_id)
         else:
             code, r = join.get_lottery()
@@ -104,12 +108,12 @@ def send_welcome(message):
     else:
         if l.find('%s' %un) == -1:
             msg_id = bot.reply_to(message,'您没有权限哦').message_id
-			time.sleep(5)
+	    time.sleep(5)
             bot.delete_message(message.chat.id,msg_id)
         else:
             r = join.del_list()
             msg_id = bot.reply_to(message,r).message_id
-			time.sleep(5)
+	    time.sleep(5)
             bot.delete_message(message.chat.id,msg_id)
 
 bot.polling()
