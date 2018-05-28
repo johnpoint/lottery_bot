f42bd3ba5fa6f5c9574c3c8712fa0f166eb19109
johnpoint <jahanngauss414@gmail.com>
2018-05-28 08:33:51 +0800

    update

--- bot2.0/db.py
+++ bot2.0/db.py
@@ -3,15 +3,14 @@
 import sqlite3
 
 def new_po(poid):
-    db = sqlite3.connect("bot.db")
+    db = sqlite3.connect("%s"%poid)
     cur = db.cursor()
-    bna = cur.execute("select name from sqlite_master where type = 'table' order by name").fetchall()
-    if bna.find('%s'%poid) ==-1:
-        cur.execute("create table ?(username text primary key);",(usn))
-        connection.commit()
-        return 0
-    else:
-        return 1
+    cur.execute('''CREATE TABLE COMPANY
+   (ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   UID            INT     NOT NULL);''')
+    connection.commit()
+    return 1
         
 def new_user(poid,uid):
     db = sqlite3.connect("user.db")
